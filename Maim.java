1968a5a30a1310db58531bd974b6b8e2a8169f05
Nastya1078 <73017059+Nastya1078@users.noreply.github.com>
2020-11-28 16:38:45 +0600

    Create Maim.java

---
+++ Maim.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.lang.Math;
+public class Main {
+
+    public static void main(String[] args) {
+
+        int past , next;
+        for (int i=0; i < 1000000; i++)
+        {
+          past = i -1;
+          next = i +1;
+          if (Math.sqrt(past)%1==0)
+              if (Math.cbrt(next)%1==0) System.out.println(i);
+        }
+    }
+}
